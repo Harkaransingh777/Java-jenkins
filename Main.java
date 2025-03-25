06c56737a15973b58d7bfd918d2101bfe6617e9f
Harkaran Singh <62829964+Harkaransingh777@users.noreply.github.com>
2025-03-26 01:36:23 +0530

    Changed

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ import java.util.Date;
 class Main{
     public static void main(String[] args){
         System.out.println("Hello World");
+        System.err.println("Changed");
         System.out.println(new Date());
     }
 }
